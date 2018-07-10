8868fdef16657ea2c7e09ccd686ea05b763d943f
oli <oli.loades@gmail.com>
2018-07-10 09:59:13 +0100

    init

---
+++ src/main/java/qa/com/account/accountproject/App.java
@@ -0,0 +1,13 @@
+package qa.com.account.accountproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
